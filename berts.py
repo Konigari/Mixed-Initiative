f99126b98342a63ed56dd5eda9bbeb88648e64b9
Saurabh Chand Ramola <31828811+saurabh-ramola@users.noreply.github.com>
2019-05-21 00:52:29 +0530

    Updated

--- berts.py
+++ berts.py
@@ -11,14 +11,17 @@ def cos_sim(a, b):
 # ctx = mx.gpu()
 pos = 0
 count = 0
+posx = 0
+countx = 0
 lists = os.listdir('./glove_filter/processedanno/')
 for name in lists:
 	file = open('./glove_filter/processedanno/'+name,'r')
 	lines = file.readlines()
 	string = ''
-	# try:
 	for line in lines:
-		string+=line.split("|")[1]+"\n"
+		if len(line.split("|")) > 1:
+			string+=line.split("|")[1]+"\n"
+
 
 	bert_abstract = string.strip().split("\n")
 	sentences = bert_abstract
@@ -43,25 +46,49 @@ for name in lists:
 			a2.append(cos_sim(sen1, sen2))
 		a1 = np.array(a1)
 		a2 = np.array(a2)
+		arr1 = a1
+		arr2 = a2
 		a1 = a1[a1 > 0.55]
 		a2 = a2[a2 > 0.55]
-		try:
+		if len(lines[index].split("|")) > 3:
 			
 			if "+" in lines[index].split("|")[3]:
-				if len(a1)-len(a2) <= 0:
 				
-					print(lines[index])
+
+				if len(a1)-len(a2) <= 0:
+					sum1 = np.sum(arr1)
+					sum2 = np.sum(arr2)
+					val = np.exp(sum1-sum2)
 					pos += 1
+					if len(lines[index].split("|")) == 5:
+						if val >= 0.5:
+							if 's' in lines[index].split("|")[4]:
+								posx += 1
+								countx += 1
+								print("Correct",lines[index],sum1-sum2)
+							else:
+								countx += 1
+								print("Wrong=>sbd",lines[index],sum1-sum2)
+
+						else:
+							if 'i' in lines[index].split("|")[4]:
+								posx += 1
+								countx += 1
+								print("Correct",lines[index],sum1-sum2)
+								
+							else:
+								countx += 1
+								print("wrong=>i",lines[index],sum1-sum2)
 
 				count += 1
 
-		except:
-			pass
+		
 
+	print("Topic Shift",pos,count)
+	print("Sub vs I",posx,countx)
 
-	print(pos,count)
-# except:
-# 	pass
+	
 
 print(pos/count)
 
+print(posx,countx)
